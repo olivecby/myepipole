98c777d4881bdc9906e18c086ffd6f3425bc9e98
olivecby <olivia.chenby@gmail.com>
2018-11-11 00:22:51 -0500

    added sounds to cubes

--- em_project/js/music.js
+++ em_project/js/music.js
@@ -1,13 +1,5 @@
-//create a synth and connect it to the master output (your speakers)
-var synth = new Tone.Synth().toMaster();
-
-//play a middle 'C' for the duration of an 8th note
-synth.triggerAttackRelease("C4", "8n");
-
-//play a note every quarter-note
-var loop = new Tone.Loop(function(time){
-	synth.triggerAttackRelease("C2", "8n", time);
-}, "4n");
-
-loop.start("1m").stop("4m");
-Tone.Transport.start();
+function play(){
+  var synth = new Tone.Synth().toMaster();
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease("C4", "8n");
+}
